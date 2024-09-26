e67e7d91d564507285dcb09459eef902ec3ec4c1
cmb347827 <v38d422ll621dem@gmail.com>
2024-09-26 17:43:59 -0400

    /*some changes to get started*/

--- static-job-listings-master.js
+++ static-job-listings-master.js
@@ -3,21 +3,50 @@
 $(window).resize(function(){
 	location.reload();
 });
+const elements = {
+	listingsContainer : document.querySelector('#js-job-listings'),
+    
+}
 
-async function getJson() {
-	const requestURL =
-	  "https://raw.githubusercontent.com/cmb347827/static-job-listings-master/refs/heads/main/data.json";
-	const request = new Request(requestURL);
-  
-	const response = await fetch(request);
-	const dataText = await response.text();
-  
-	const data = JSON.parse(dataText);
-	console.log(data);
-	
-  }
+
+
+async function fetchAsync () {
+	// await response of fetch call
+	let response = await fetch('https://raw.githubusercontent.com/cmb347827/static-job-listings-master/refs/heads/main/data.json');
+	// only proceed once promise is resolved
+	const data= await response.json()
+	// only proceed once second promise is resolved
+	addListings(data);
+}
+
+
+const addListings=(data)=>{
+	  //console.log('in addlistings:',data);
+      data.forEach((item,index)=>{
+		elements.listingsContainer.innerHTML += `
+		    <section class='d-flex flex-md-row flex-column'>
+		      <img src='${item.logo}' alt=''>
+			  <div class='d-flex'>
+		        <div>${item.company} </div>
+				<div>${item.new ? 'NEW!' : ''} </div>
+				<div>${item.featured ? 'FEATURED' :''}</div>
+			  </div>
+			  <div>
+			     ${item.position}
+			  </div>
+			  <div>
+			     <div>${}</div>
+                 <div>${}</div>
+				 <div>${}</div>
+			  </div>
+		    </section>
+	    `;
+	  });
+};
 
 $(window).on('load',function(){
-     getJson();
+    fetchAsync();
+	
     
+
 });
